7ca57b710841fdac2b18819cbc323b29d4b51b30
liam waaga <liamgames505@gmail.com>
2024-10-02 17:36:42 -0700

    color fine tuning

--- src/main.cpp
+++ src/main.cpp
@@ -53,13 +53,14 @@ int main (int argc, char* argv[]) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     #endif
 
     // begin initializing glfw window
-    GLFWwindow* window = glfwCreateWindow(1366, 768, "HELLO WORLD", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(128, 128, "HELLO WORLD", NULL, NULL);
     if (window == NULL) {
         std::cout << "FATAL: failed to create glfw window\n";
         glfwTerminate();
@@ -73,20 +74,40 @@ int main (int argc, char* argv[]) {
         return 1;
     }
     // set the viewport size
-    glViewport(0, 0, 1366, 768);
+    glViewport(0, 0, 64, 64);
 
     // set the function to be called on the resize of the given window
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     
     Data data;
-    data.red = 0;
-    data.green = 0;
-    data.blue = 0;
+
+    // parse command line arguments
+    if (argc == 4) {
+        data.red = (float) std::stoi (argv[1]) / 255;
+        data.green = (float) std::stoi (argv[2]) / 255;
+        data.blue = (float) std::stoi (argv[3]) / 255;
+    }
     // finished window initialization
     // main event loop
     while (!glfwWindowShouldClose(window)) {
-
+        
         process_input(window, data);
+        if (data.red > 1) {
+            data.red = 1;
+        } else if (data.red < 0) {
+            data.red = 0;
+        }
+        if (data.green > 1) {
+            data.green = 1;
+        } else if (data.green < 0) {
+            data.green = 0;
+        }
+        if (data.blue > 1) {
+            data.blue = 1;
+        } else if (data.blue < 0) {
+            data.blue = 0;
+        }
+        std::cout << data.red*255 << ", " << data.green*255 << ", " << data.blue*255 << "\n";
         glClearColor(data.red, data.green, data.blue, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
